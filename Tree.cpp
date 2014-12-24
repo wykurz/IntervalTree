f6a189398a02fd94348f27ebcde57503a9373311
Mateusz Wykurz <wykurz@gmail.com>
2014-12-24 02:00:32 +0100

    Added a suite of tests

--- Tree.cpp
+++ Tree.cpp
@@ -62,47 +62,72 @@ Test::Predicate someCheck()
     return issues.predicate;
 }
 
-BOOST_AUTO_TEST_CASE(Test01)
+BOOST_AUTO_TEST_CASE(Basic)
 {
     // [0, 0], [0, 1], [0, 2], [0, 3], [0, 4] - 5 : 5
     //         [1, 1], [1, 2], [1, 3], [1, 4] - 4 : 9
     //                 [2, 2], [2, 3], [2, 4] - 3 : 12
     //                         [3, 3], [3, 4] - 2 : 14
     //                                 [4, 4] - 1 : 15
-    Tree t(Interval(1, 5));
+    Tree t(Interval(0, 4));
     {
         const Interval i = t.findNthInterval(6);
         BOOST_CHECK_EQUAL(Interval(1, 2), i);
-        t.removeInterval(i);
+        BOOST_CHECK(t.removeInterval(i));
     }
     // [0, 0], [3, 3], [3, 4], [4, 4]
     BOOST_CHECK_EQUAL(4, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
         BOOST_CHECK_EQUAL(Interval(3, 3), i);
-        t.removeInterval(i);
+        BOOST_CHECK(t.removeInterval(i));
     }
     // [0, 0], [4, 4]
     BOOST_CHECK_EQUAL(2, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
         BOOST_CHECK_EQUAL(Interval(4, 4), i);
-        t.removeInterval(i);
+        BOOST_CHECK(t.removeInterval(i));
     }
     // [0, 0]
     BOOST_CHECK_EQUAL(1, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
         BOOST_CHECK_EQUAL(Interval(0, 0), i);
-        t.removeInterval(i);
+        BOOST_CHECK(t.removeInterval(i));
     }
     // empty
     BOOST_CHECK_EQUAL(0, t.countIntervals());
 }
 
-BOOST_AUTO_TEST_CASE(Test02)
+BOOST_AUTO_TEST_CASE(RemoveInterval)
 {
-    BOOST_CHECK(true);
+    Tree t(Interval(0, 4));
+    // [0, 4]
+    BOOST_CHECK(!t.removeInterval(Interval(0, 5)));
+    BOOST_CHECK(!t.removeInterval(Interval(5, 5)));
+    BOOST_CHECK( t.removeInterval(Interval(1, 3)));
+    // [0, 0], [4, 4]
+    BOOST_CHECK(!t.removeInterval(Interval(0, 1)));
+    BOOST_CHECK(!t.removeInterval(Interval(3, 4)));
+    BOOST_CHECK( t.removeInterval(Interval(0, 0)));
+    // [4, 4]
+    BOOST_CHECK(!t.removeInterval(Interval(5, 4)));
+    BOOST_CHECK( t.removeInterval(Interval(4, 4)));
+}
+
+BOOST_AUTO_TEST_CASE(Large)
+{
+    Index N = 1e6;
+    Tree t(Interval(0, N - 1));
+
+    // Remove all [<even>, <even>]
+    for (Index i = 0; i < N; i += 2)
+    {
+        BOOST_CHECK(t.removeInterval(Interval(i, i)));
+    }
+    BOOST_CHECK_EQUAL(N / 2, t.countIntervals());
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
