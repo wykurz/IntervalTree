4ee9c7dba4aa154e218c3c052caab3ffef6a3c46
Mateusz Wykurz <wykurz@gmail.com>
2014-12-24 14:14:32 +0100

    Basic implementation; no tree rebalancing; not passing all tests

--- Tree.cpp
+++ Tree.cpp
@@ -14,42 +14,244 @@ typedef std::size_t             Count;
 typedef std::size_t             Index;
 typedef std::pair<Index, Index> Interval;
 
+#define DEBUG
+
+#ifdef DEBUG
+#define debug(x) std::cerr << "DEBUG: " << x << std::endl
+#else
+#define debug(x)
+#endif
+
 namespace std {
 
-    inline std::ostream& operator<<(std::ostream& stream_, const Interval& i_)
+    inline ostream& operator<<(ostream& stream_, const Interval& interval_)
     {
-        stream_ << "[" << i_.first << ", " << i_.second << "]";
+        stream_ << "[" << interval_.first << ", " << interval_.second << "]";
         return stream_;
     }
 
 }
 
+Count numAllIntervals(const Interval& interval_)
+{
+    if (interval_.second < interval_.first)
+    {
+        return 0;
+    }
+    Count n = interval_.second - interval_.first + 1;
+    return n * (n + 1) / 2;
+}
+
+Interval findNthContInterval(Count nth_, const Interval& interval_)
+{
+    // TODO: too slow
+    Index A = interval_.first;
+    Index B = interval_.second;
+    Index a = A;
+    Index b = A;
+    Interval interval(interval_.first, interval_.first);
+    while (0 < nth_--)
+    {
+        if (b < B)
+        {
+            ++b;
+        }
+        else
+        {
+            assert(a < B);
+            ++a;
+            b = a;
+        }
+    }
+    return Interval(a, b);
+}
+
+bool intervalContains(const Interval& a_, const Interval& b_)
+{
+    return a_.first <= b_.first && b_.second <= a_.second;
+}
+
+bool intervalStrictlyWithin(const Interval& a_, const Interval& b_)
+{
+    return a_.first < b_.first && b_.second < a_.second;
+}
+
 class Tree
 {
   public:
-    Tree(const Interval& i_);
+    struct Node
+    {
+        Interval interval;
+        Index    left;
+        Index    right;
+        Count    count;
+    };
+
+    typedef std::vector<Node> NodeList;
+
+    Tree(const Interval& interval_);
 
     Count countIntervals() const;
+    // NOTE: zero-indexed
     Interval findNthInterval(Count nth_) const;
-    bool removeInterval(const Interval& i_);
+    bool complementOf(const Interval& interval_);
+
+  private:
+    Interval findNthIntervalImpl(Count nth_, Index i_) const;
+    Index addNode(const Interval& interval_);
+    bool complementOfImpl(const Interval& interval_, Index i_);
+
+    NodeList _nodes;
 };
 
-Tree::Tree(const Interval& i_)
+
+namespace std {
+
+    inline ostream& operator<<(ostream& stream_, const Tree::Node& node_)
+    {
+        stream_ << "\n"
+                << "{ interval : " << node_.interval << "\n"
+                << "  left     : " << node_.left << "\n"
+                << "  right    : " << node_.right << "\n"
+                << "  count    : " << node_.count << " }";
+        return stream_;
+    }
+
+}
+
+Tree::Tree(const Interval& interval_)
 {
+    _nodes.push_back({interval_, 0, 0, numAllIntervals(interval_)});
 }
 
 Count Tree::countIntervals() const
 {
-    return 0;
+    return _nodes[0].count;
 }
 
 Interval Tree::findNthInterval(Count nth_) const
 {
-    return Interval(0, 0);
+    return findNthIntervalImpl(nth_, 0);
 }
 
-bool Tree::removeInterval(const Interval& i_)
+bool Tree::complementOf(const Interval& interval_)
 {
+    return complementOfImpl(interval_, 0);
+}
+
+Interval Tree::findNthIntervalImpl(Count nth_, Index i_) const
+{
+    const Node& node(_nodes[i_]);
+    if (0 != node.left)
+    {
+        const Count leftCount = _nodes[node.left].count;
+        if (nth_ < leftCount)
+        {
+            return findNthIntervalImpl(nth_, node.left);
+        }
+        else
+        {
+            nth_ -= leftCount;
+        }
+    }
+    if (0 != node.right)
+    {
+        return findNthIntervalImpl(nth_, node.right);
+    }
+    assert(nth_ < node.count);
+    return findNthContInterval(nth_, node.interval);
+}
+
+Index Tree::addNode(const Interval& interval_)
+{
+    Index i = _nodes.size();
+    _nodes.push_back({interval_, 0, 0, numAllIntervals(interval_)});
+    debug("Created new node " << i << ":" << _nodes[i]);
+    return i;
+}
+
+bool Tree::complementOfImpl(const Interval& interval_, Index i_)
+{
+    const Node& node(_nodes[i_]);
+    debug("Processing complement of node " << i_ << ":" << node);
+    if (0 != node.left && intervalContains(_nodes[node.left].interval, interval_))
+    {
+        if (complementOfImpl(interval_, node.left))
+        {
+            Node& changeNode(_nodes[i_]);
+            changeNode.count = _nodes[changeNode.left].count;
+            if (0 != changeNode.right)
+            {
+                changeNode.count += _nodes[changeNode.right].count;
+            }
+            return true;
+        }
+        return false;
+    }
+    if (0 != node.right && intervalContains(_nodes[node.right].interval, interval_))
+    {
+        if (complementOfImpl(interval_, node.right))
+        {
+            Node& changeNode(_nodes[i_]);
+            changeNode.count = _nodes[changeNode.right].count;
+            if (0 != changeNode.left)
+            {
+                changeNode.count += _nodes[changeNode.left].count;
+            }
+            return true;
+        }
+        return false;
+    }
+    if (!intervalContains(node.interval, interval_))
+    {
+        debug(node.interval << " does not contain " << interval_);
+        return false;
+    }
+    if (0 == node.left && 0 == node.right)
+    {
+        const Index a  = node.interval.first;
+        const Index b  = node.interval.second;
+        const Index ap = interval_.first;
+        const Index bp = interval_.second;
+        if (intervalStrictlyWithin(node.interval, interval_))
+        {
+            const Interval  leftInterval(a,      ap - 1);
+            const Interval rightInterval(bp + 1, b     );
+
+            const Index  leftIndex = addNode( leftInterval);
+            const Index rightIndex = addNode(rightInterval);
+
+            Node& changeNode(_nodes[i_]);
+            changeNode.left  = leftIndex;
+            changeNode.right = rightIndex;
+            changeNode.count = _nodes[leftIndex].count + _nodes[rightIndex].count;
+        }
+        else
+        {
+            Node& changeNode(_nodes[i_]);
+            if (a < ap)
+            {
+                assert(b <= bp);
+                changeNode.interval = Interval(a, ap - 1);
+            }
+            else if (bp < b)
+            {
+                assert(ap <= a);
+                changeNode.interval = Interval(bp + 1, b);
+            }
+            else
+            {
+                // Remove whole node
+                assert(ap == a);
+                assert(bp == b);
+                changeNode.interval = Interval(1, 0); // invalid
+            }
+            changeNode.count = numAllIntervals(changeNode.interval);
+        }
+        const Node& updatedNode(_nodes[i_]);
+        debug("Updated node " << i_ << ":" << updatedNode);
+        return true;
+    }
     return false;
 }
 
@@ -62,6 +264,39 @@ Test::Predicate someCheck()
     return issues.predicate;
 }
 
+// bool intervalContains(const Interval& a_, const Interval& b_)
+// bool intervalStrictlyWithin(const Interval& a_, const Interval& b_)
+
+BOOST_AUTO_TEST_CASE(Preliminary)
+{
+    BOOST_CHECK_EQUAL(Interval(0, 0), findNthContInterval(0, Interval(0, 0)));
+    BOOST_CHECK_EQUAL(Interval(0, 0), findNthContInterval(0, Interval(0, 1)));
+    BOOST_CHECK_EQUAL(Interval(0, 0), findNthContInterval(0, Interval(0, 5)));
+    BOOST_CHECK_EQUAL(Interval(4, 7), findNthContInterval(8, Interval(3, 7)));
+    BOOST_CHECK_EQUAL(Interval(2, 2), findNthContInterval(2, Interval(1, 2)));
+    BOOST_CHECK_EQUAL(Interval(1e3, 1e3), findNthContInterval(500499, Interval(1, 1e3)));
+
+    BOOST_CHECK( intervalContains(Interval(0, 4), Interval(0, 0)));
+    BOOST_CHECK( intervalContains(Interval(0, 4), Interval(4, 4)));
+    BOOST_CHECK( intervalContains(Interval(0, 4), Interval(0, 4)));
+    BOOST_CHECK( intervalContains(Interval(0, 4), Interval(1, 3)));
+    BOOST_CHECK(!intervalContains(Interval(0, 4), Interval(1, 5)));
+    BOOST_CHECK(!intervalContains(Interval(0, 4), Interval(5, 5)));
+    BOOST_CHECK(!intervalContains(Interval(2, 4), Interval(0, 1)));
+
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(0, 4), Interval(0, 0)));
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(0, 4), Interval(4, 4)));
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(0, 4), Interval(0, 4)));
+    BOOST_CHECK( intervalStrictlyWithin(Interval(0, 4), Interval(1, 1)));
+    BOOST_CHECK( intervalStrictlyWithin(Interval(0, 4), Interval(1, 2)));
+    BOOST_CHECK( intervalStrictlyWithin(Interval(0, 4), Interval(1, 3)));
+    BOOST_CHECK( intervalStrictlyWithin(Interval(0, 4), Interval(2, 3)));
+    BOOST_CHECK( intervalStrictlyWithin(Interval(0, 4), Interval(3, 3)));
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(0, 4), Interval(1, 5)));
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(0, 4), Interval(5, 5)));
+    BOOST_CHECK(!intervalStrictlyWithin(Interval(2, 4), Interval(0, 1)));
+}
+
 BOOST_AUTO_TEST_CASE(Basic)
 {
     // [0, 0], [0, 1], [0, 2], [0, 3], [0, 4] - 5 : 5
@@ -72,48 +307,48 @@ BOOST_AUTO_TEST_CASE(Basic)
     Tree t(Interval(0, 4));
     {
         const Interval i = t.findNthInterval(6);
-        BOOST_CHECK_EQUAL(Interval(1, 2), i);
-        BOOST_CHECK(t.removeInterval(i));
+        BOOST_REQUIRE_EQUAL(Interval(1, 2), i);
+        BOOST_REQUIRE(t.complementOf(i));
     }
     // [0, 0], [3, 3], [3, 4], [4, 4]
-    BOOST_CHECK_EQUAL(4, t.countIntervals());
+    BOOST_REQUIRE_EQUAL(4, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
-        BOOST_CHECK_EQUAL(Interval(3, 3), i);
-        BOOST_CHECK(t.removeInterval(i));
+        BOOST_REQUIRE_EQUAL(Interval(3, 3), i);
+        BOOST_REQUIRE(t.complementOf(i));
     }
     // [0, 0], [4, 4]
-    BOOST_CHECK_EQUAL(2, t.countIntervals());
+    BOOST_REQUIRE_EQUAL(2, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
-        BOOST_CHECK_EQUAL(Interval(4, 4), i);
-        BOOST_CHECK(t.removeInterval(i));
+        BOOST_REQUIRE_EQUAL(Interval(4, 4), i);
+        BOOST_REQUIRE(t.complementOf(i));
     }
     // [0, 0]
-    BOOST_CHECK_EQUAL(1, t.countIntervals());
+    BOOST_REQUIRE_EQUAL(1, t.countIntervals());
     {
         const Interval i = t.findNthInterval(1);
-        BOOST_CHECK_EQUAL(Interval(0, 0), i);
-        BOOST_CHECK(t.removeInterval(i));
+        BOOST_REQUIRE_EQUAL(Interval(0, 0), i);
+        BOOST_REQUIRE(t.complementOf(i));
     }
     // empty
-    BOOST_CHECK_EQUAL(0, t.countIntervals());
+    BOOST_REQUIRE_EQUAL(0, t.countIntervals());
 }
 
 BOOST_AUTO_TEST_CASE(RemoveInterval)
 {
     Tree t(Interval(0, 4));
     // [0, 4]
-    BOOST_CHECK(!t.removeInterval(Interval(0, 5)));
-    BOOST_CHECK(!t.removeInterval(Interval(5, 5)));
-    BOOST_CHECK( t.removeInterval(Interval(1, 3)));
+    BOOST_CHECK(!t.complementOf(Interval(0, 5)));
+    BOOST_CHECK(!t.complementOf(Interval(5, 5)));
+    BOOST_CHECK( t.complementOf(Interval(1, 3)));
     // [0, 0], [4, 4]
-    BOOST_CHECK(!t.removeInterval(Interval(0, 1)));
-    BOOST_CHECK(!t.removeInterval(Interval(3, 4)));
-    BOOST_CHECK( t.removeInterval(Interval(0, 0)));
+    BOOST_CHECK(!t.complementOf(Interval(0, 1)));
+    BOOST_CHECK(!t.complementOf(Interval(3, 4)));
+    BOOST_CHECK( t.complementOf(Interval(0, 0)));
     // [4, 4]
-    BOOST_CHECK(!t.removeInterval(Interval(5, 4)));
-    BOOST_CHECK( t.removeInterval(Interval(4, 4)));
+    BOOST_CHECK(!t.complementOf(Interval(5, 4)));
+    BOOST_CHECK( t.complementOf(Interval(4, 4)));
 }
 
 BOOST_AUTO_TEST_CASE(Large)
@@ -124,7 +359,7 @@ BOOST_AUTO_TEST_CASE(Large)
     // Remove all [<even>, <even>]
     for (Index i = 0; i < N; i += 2)
     {
-        BOOST_CHECK(t.removeInterval(Interval(i, i)));
+        BOOST_CHECK(t.complementOf(Interval(i, i)));
     }
     BOOST_CHECK_EQUAL(N / 2, t.countIntervals());
 
